16aa3fac9b11dc9088ce262456df11c18eb98524
Pranay Prakash <pranay.gp@gmail.com>
2016-09-13 10:14:34 -0500

    Test Travis

--- src/artmtc.js
+++ src/artmtc.js
@@ -1,11 +1,11 @@
-module.exports = {
-  add: function(x, y) { return x + y },
-  sub: function(x, y) { return this.subtract(x,y) },
-  subtract: function(x, y) {return x - y},
-  mult: function(x, y) { return this.multiply(x, y)},
-  multiply: function(x, y) {return x*y },
-  div: function(x, y) {return this.divide(x, y) },
-  divide: function(x, y) {return x/y}
-};
+//@flow
 
-export default module.exports;
+module.exports = {
+  add: function(x : number, y : number):number { return x + y },
+  sub: function(x : number, y : number):number { return this.subtract(x,y) },
+  subtract: function(x : number, y : number):number {return x - y},
+  mult: function(x : number, y : number):number { return this.multiply(x, y)},
+  multiply: function(x : number, y : number): number {return x*y },
+  div: function(x : number, y : number): number {return this.divide(x, y) },
+  divide: function(x : number, y : number): number {return x/y}
+};
