72224a134d8687bb0a51e082a026345c7fc9e0f6
Pranay Prakash <pranay.gp@gmail.com>
2016-09-13 09:08:01 -0500

    Updates?

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require("gulp");
-var babel = require('gulp-babel');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 gulp.task("default",['build']);
 
 gulp.task("build", () => {
-  return gulp.src("src/artmtc.js")
+  return gulp.src("src/**/*.js")
     .pipe(babel())
+    .pipe(uglify())
     .pipe(gulp.dest("dist"))
 })
